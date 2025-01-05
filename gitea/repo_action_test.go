a239deff5a96c327c7d6f58c2a61eb95a5c7ac17
Marek Mościchowski <marek@noreply.gitea.com>
2025-01-05 03:54:59 +0000

    Add support for repository Action Variables (#677)
    Add support for repository Action Variables:
    
    - Create
    - Read
    - Update
    - Delete
    
    Also added deleting for repository Action Secret
    
    Co-authored-by: Marek Mościchowski <marek.moscichowski@miquido.com>
    Co-authored-by: techknowlogick <techknowlogick@noreply.gitea.com>
    Reviewed-on: https://gitea.com/gitea/go-sdk/pulls/677
    Reviewed-by: techknowlogick <techknowlogick@noreply.gitea.com>
    Co-authored-by: Marek Mościchowski <marek@noreply.gitea.com>
    Co-committed-by: Marek Mościchowski <marek@noreply.gitea.com>

--- gitea/repo_action_test.go
+++ gitea/repo_action_test.go
@@ -38,4 +38,58 @@ func TestCreateRepoActionSecret(t *testing.T) {
 	secrets, _, err := c.ListRepoActionSecret(newRepo.Owner.UserName, newRepo.Name, ListRepoActionSecretOption{})
 	assert.NoError(t, err)
 	assert.Len(t, secrets, 1)
+
+	// delete secret
+	resp, err = c.DeleteRepoActionSecret(newRepo.Owner.UserName, newRepo.Name, "test")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+
+	// confirm that secret was deleted
+	secrets, _, err = c.ListRepoActionSecret(newRepo.Owner.UserName, newRepo.Name, ListRepoActionSecretOption{})
+	assert.NoError(t, err)
+	assert.Len(t, secrets, 0)
+}
+
+func TestCreateRepoActionVariable(t *testing.T) {
+	log.Println("== TestCreateRepoActionVariable ==")
+	c := newTestClient()
+
+	user := createTestUser(t, "repo_action_variable_user", c)
+	c.SetSudo(user.UserName)
+	newRepo, _, err := c.CreateRepo(CreateRepoOption{
+		Name: "test_variable",
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, newRepo)
+
+	// create variable
+	resp, err := c.CreateRepoActionVariable(newRepo.Owner.UserName, newRepo.Name, "TEST_VAR", "test value")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+
+	// get variable
+	variable, resp, err := c.GetRepoActionVariable(newRepo.Owner.UserName, newRepo.Name, "TEST_VAR")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "test value", variable.Value)
+
+	// update variable
+	resp, err = c.UpdateRepoActionVariable(newRepo.Owner.UserName, newRepo.Name, "TEST_VAR", "new updated value")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+
+	// get updated variable
+	variable, resp, err = c.GetRepoActionVariable(newRepo.Owner.UserName, newRepo.Name, "TEST_VAR")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "new updated value", variable.Value)
+
+	// delete variable
+	resp, err = c.DeleteRepoActionVariable(newRepo.Owner.UserName, newRepo.Name, "TEST_VAR")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+
+	// 404 when get variable
+	_, resp, _ = c.GetRepoActionVariable(newRepo.Owner.UserName, newRepo.Name, "TEST_VAR")
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
 }
